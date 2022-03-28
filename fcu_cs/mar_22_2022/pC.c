3c92922246804a9b3c2b1eb30370ec9b112c02c4
Alec Wu <980124jack@gmail.com>
2022-03-29 01:13:20 +0800

    fix runtime error

--- fcu_cs/mar_22_2022/pC.c
+++ fcu_cs/mar_22_2022/pC.c
@@ -10,31 +10,31 @@ int main(){
     int n;
 
     scanf("%d", &n);
-        char str[256];
+    char str[256];
 
-        for(int ss=0; ss<n; ss++){
-            scanf("%s", str);
-            
-            char c;
-            int cnt = 0;
-            for(int i=0; i<strlen(str); i++){
-                if(isalpha(str[i])){
-                    c = str[i];
-                }
-                else{
-                    cnt *= 10;
-                    cnt += str[i] - '0';
+    for(int ss=0; ss<n; ss++){
+        scanf("%s", str);
+        
+        char c;
+        int cnt = 0;
+        for(int i=0; i<strlen(str); i++){
+            if(isalpha(str[i])){
+                c = str[i];
+            }
+            else{
+                cnt *= 10;
+                cnt += str[i] - '0';
 
-                    if(isalpha(str[i+1]) || i+1 == strlen(str)){
-                        for(int j=0; j<cnt; j++){
-                            printf("%c", c);
-                        }
-                        cnt = 0;
+                if(isalpha(str[i+1]) || i+1 == strlen(str)){
+                    for(int j=0; j<cnt; j++){
+                        printf("%c", c);
                     }
+                    cnt = 0;
                 }
             }
-
-            printf("\n");
         }
+
+        printf("\n");
+    }
 }
 
