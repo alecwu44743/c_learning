ca608067ce454ae3aaf79e53f3b7b5f62adc2bd5
Alec Wu <980124jack@gmail.com>
2022-03-28 23:25:14 +0800

    update oj answer March 22, 2022

--- fcu_cs/mar_22_2022/pD.c
+++ fcu_cs/mar_22_2022/pD.c
@@ -5,36 +5,28 @@
 #include <math.h>
 #include <stdbool.h>
 
+int check(int arr[], int i, int sum, int *N, int *K){
+    if(i == *N) return abs(sum) % (*K);
+
+    if(check(arr, i+1, sum+arr[i], N, K) == 0) return 0;
+    if(check(arr, i+1, sum-arr[i], N, K) == 0) return 0;
+
+    return 1;        
+}
+
 int main(){
     int nn;
     int n, k;
-    int num[100];
-    int dp[1000][1000];
+    int arr[100];
 
-    scanf("%d", &nn);
+    while(scanf("%d", &nn)){
         for(int ii=0; ii<nn; ii++){
             scanf("%d %d", &n, &k);
+            
+            for(int i=0; i<n; i++) scanf("%d", &arr[i]);
 
-            for(int i=0; i<n; i++){
-                scanf("%d", &num[i]);
-                num[i] = abs(num[i]) % k;
-            }
-
-            memset(dp, 0, sizeof(dp)), dp[0][0] = 1;
-            for(int i=0; i<n; i++){
-                for(int j=0; j<k; j++){
-                    if(dp[i][j]){
-                        dp[i+1][(j+num[i]+k) % k] = 1;
-                        dp[i+1][(j-num[i]+k) % k] = 1;
-                    }
-                }
-            }
-
-            if(dp[n][0]){
-                printf("Divisible\n");
-            }
-            else{
-                printf("Not divisible\n");
-            }
+            if(check(arr, 0, 0, &n, &k) == 0) printf("Divisible\n");
+            else printf("Not divisible\n");
         }
+    }
 }
