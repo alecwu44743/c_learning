88036f0633de8bfd4fb34ebba54f507b6b04c994
Alec Wu <980124jack@gmail.com>
2022-03-31 20:54:21 +0800

    pC - add notes Mar 31, 2022

--- fcu_cs/mar_29_2022/pC.cpp
+++ fcu_cs/mar_29_2022/pC.cpp
@@ -3,6 +3,26 @@
 #include <string>
 using namespace std;
 
+// 觀察 BST 的前八項：
+// 0                1
+// 01               101
+// 01101            10101101
+// 0110110101101    101011010110110101101
+// 而每個字串的長度為
+// 1                           1
+// 2                           3
+// 5                           8
+// 13                         21
+// 為典型的費氏數列
+//
+// 此時我們替換 BST 的每一項：
+// X:                     Y:
+// X0 - X0                Y0 - Y0
+// X1 - X0Y0              Y1 - Y0X0Y0
+// X2 - X1Y1              Y2 - Y1X1Y1
+// X3 - X2Y2              Y3 - Y2X2Y2
+// 其中 Xi = Xi-1Yi-1 、 Yi = Yi-1Xi-1Yi-1 ，而 X0 = 0 、 Y0 = 1
+
 unsigned int fib[24][2] = { {1, 1} };
 string BFSs[4] = {"0", "1", "01", "101"};
 
@@ -33,9 +53,9 @@ int main() {
 			buffer = n;
 
 			while(buffer >= 4){
-				sum = 0;
-				if(buffer % 2){
-					if(nowIndex < fib[(buffer / 2) - 1][1]){
+				sum = 0; // pre-length
+				if(buffer % 2){ // if buffer is at the Y
+					if(nowIndex < fib[(buffer / 2) - 1][1]){ // check if nowIndex is in the Y((buffer / 2) - 1)
 						buffer -= 2;
 						continue;
 					}
@@ -44,8 +64,9 @@ int main() {
 
 				for(int j=0; j<2; j++){
 					if(nowIndex < sum + fib[(buffer / 2) - 1][j]){
-						buffer = (buffer - 2 - (buffer % 2)) + j;
-						nowIndex -= sum;
+						buffer = (buffer - 2 - (buffer % 2)) + j; // buffer % 2 ->  check X or Y
+						                                          // + j -> ??
+						nowIndex -= sum; // minus the preceding length
 						break;
 					}
 					sum += fib[(buffer / 2) - 1][j];
