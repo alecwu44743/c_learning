48bf2f8f3ec7b4fd22ecaf970763e32556813d41
Alec Wu <980124jack@gmail.com>
2022-04-11 22:23:17 +0800

    midterm-exam note (FCU OJ Problems - pC, Week 6) Apr 11, 2022

--- fcu_cs/exam/apr_2022_mid.c
+++ fcu_cs/exam/apr_2022_mid.c
@@ -258,6 +258,42 @@ void easy_find_smallest_period(){
 
 
 
+// FCU OJ Problems - pC, Week 6
+void str_decode(){
+    int n;
+
+    while(scanf("%d", &n)){
+        char str[256];
+
+        for(int ss=0; ss<n; ss++){
+            scanf("%s", str);
+            
+            char c;
+            int cnt = 0;
+            for(int i=0; i<strlen(str); i++){
+                if(isalpha(str[i])){
+                    c = str[i];
+                }
+                else{
+                    cnt *= 10;
+                    cnt += str[i] - '0';
+
+                    if(isalpha(str[i+1]) || i+1 == strlen(str)){
+                        for(int j=0; j<cnt; j++){
+                            printf("%c", c);
+                        }
+                        cnt = 0;
+                    }
+                }
+            }
+
+            printf("\n");
+        }
+    }
+}
+
+
+
 // d670: 10921 - Find the Telephone - https://zerojudge.tw/ShowProblem?problemid=d670
 void phone_number(){
     char str[100];
