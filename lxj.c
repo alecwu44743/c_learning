19c695ddf14ebd5b029417d6b938d08cbe8600b1
Alec Wu <980124jack@gmail.com>
2022-04-13 20:47:56 +0800

    lxj.c Apr 13, 2022

---
+++ lxj.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct st{
+    int n;
+    char c;
+};
+
+int main(){
+    // int a[9][8][7];
+    // int i, j, k;
+    // for (i=0; i<9; i++)
+    //     for (j=0; j<8; j++)
+    //         for (k=0; k<7; k++) 
+    //             a[i][j][k] = i + j + k;
+
+    // printf("a+4: %d %p\n", *(*(*(a+4))), a+4);
+    // printf("*(a+4)+5: %d %p\n", *(a+4)+5, a+4);
+    // printf("**(a+5)+1: %d %p\n", **(a+5)+1, **(a+5)+1);
+    // printf("*(*(a+5)+3)+3: %d %p\n", *(*(a+5)+3)+3, *(*(a+5)+3)+3);
+    // printf("*(*(*(a+6)+5)+4): %d %p\n", *(*(*(a+6)+5)+4), *(*(*(a+6)+5)+4));
+    // printf("a+4: %d %p", *(a+4), a+4);
+    // printf("a+4: %d %p", *(a+4), a+4);
+    // printf("a+4: %d %p", *(a+4), a+4);
+
+    // https://blog.xuite.net/tzeng015/twblog/113273051
+    // struct st ssss;
+    // printf("%d\n", sizeof(ssss));
+
+
+}
