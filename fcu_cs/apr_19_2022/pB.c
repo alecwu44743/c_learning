43130785a25bcdc6e7e54dfee2aa2903d9070c1d
Alec Wu <980124jack@gmail.com>
2022-04-19 12:35:55 +0800

    pB - reference, Apr 19, 2022

--- fcu_cs/apr_19_2022/pB.c
+++ fcu_cs/apr_19_2022/pB.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// reference - https://www.csie.ntu.edu.tw/~b98902112/cpp_and_algo/cpp02/euclidean_algorithm.html
 int gcd(int x, int y){
     // int n = x*y;
     while(y != 0){
