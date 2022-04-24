0d16ade3899d36d4b634e5ac4e38f2985657ee13
Alec Wu <980124jack@gmail.com>
2022-04-24 15:46:04 +0800

    p11.c done. Apr 24, 2022

---
+++ yu-jie_wu/p11.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    for(int i=1; i<10; i++){
+        for(int j=1; j<10; j++){
+            printf("%d * %d = %d\n", i, j, i*j);
+        }
+        printf("\n");
+    }
+}
