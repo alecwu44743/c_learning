74641cb3f7d160dabaa02c6a2776367f88338bf9
Alec Wu <980124jack@gmail.com>
2022-05-09 15:12:37 +0800

    pB (.c) May 9, 2022

---
+++ fcu_cs/may_03_2022/pB.c
@@ -0,0 +1,26 @@
+// 10142 - Australian Voting
+// https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=1083
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main(){
+    int t, n;
+    int remains, voteN, votes[1000][20], count[20];
+    int index, half, _min, _max;
+
+    char candidate[20][101];
+    char buffer[20][101];
+    char input[20];
+    int kickedoff[20];
+
+    scanf("%d", &t);
+    while(t--){
+        
+    }
+}
