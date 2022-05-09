20c49e94f60a8b95737bea48b7b6f15997728fb4
Alec Wu <980124jack@gmail.com>
2022-05-09 16:34:07 +0800

    qiao.xu__.c - fixed May 9, 2022

--- qiao.xu__.c
+++ qiao.xu__.c
@@ -3,6 +3,29 @@
 #include <string.h>
 #include <math.h>
 
+int bubble_sort(char *str){
+    int pass = 0;
+
+    for(int i=0; i<strlen(str); i++){
+        for(int j=0; j<strlen(str)-1-i; j++){
+            if(str[j] > str[j+1]){
+                char tmp = str[j];
+                str[j] = str[j+1];
+                str[j+1] = tmp;
+
+                pass++;
+                printf("%-2d pass =>", pass);
+                for(int k=0; k<strlen(str); k++){
+                    printf("%c ", str[k]);
+                }
+                printf("\n");
+            }
+        }
+    }
+
+    return pass;
+}
+
 
 int main(){
 
@@ -17,23 +40,8 @@ int main(){
         }
         printf("\n");
 
-        int pass = 0;
-        for(int i=0; i<strlen(str); i++){
-            for(int j=0; j<strlen(str)-1-i; j++){
-                if(str[j] > str[j+1]){
-                    char tmp = str[j];
-                    str[j] = str[j+1];
-                    str[j+1] = tmp;
-
-                    pass++;
-                    printf("%-2d pass =>", pass);
-                    for(int k=0; k<strlen(str); k++){
-                        printf("%c ", str[k]);
-                    }
-                    printf("\n");
-                }
-            }
-        }
+        int ps = bubble_sort(str);
+        printf("%d\n", ps);
 
         printf("Please enter blablabla....: ");
     }
