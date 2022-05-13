a96c74d9709d853a48170a787a01aa07419b6844
Alec Wu <980124jack@gmail.com>
2022-05-13 14:47:06 +0800

    qiao-mei, output format. May 13, 2022

--- qiao.xu__.c
+++ qiao.xu__.c
@@ -7,10 +7,11 @@
 
 int f1Arr[N+1];
 int f2Arr[N+1];
-int ans[2*N+1] = {0};
 
 
 void arrMultipl(){
+    int ans[2*N+1] = {0};
+
     for(int i=0; i<N+1; i++){
         for(int j=0; j<N+1; j++){
             ans[i+j] += f1Arr[i] * f2Arr[j]; 
@@ -18,7 +19,7 @@ void arrMultipl(){
     }
 
     printf("f1 * f2 = %d^%d", ans[N*2], N*2);
-    for(int i=N*2; i>0; i--){
+    for(int i=N*2-1; i>0; i--){
         if(ans[i] >= 0){
             printf(" + %dx^%d", ans[i], i);
         }
@@ -39,14 +40,14 @@ int main(){
     char str[9];
 
     while(1){
-        printf("Please enter blablabla...:");
+        printf("Please enter blablabla...: ");
 
         if(scanf("%s", str) == EOF){
             break;
         }
         printf("This is student ID: %s... midterm exam blablabla...\n", str);
         
-        printf("Please enter f1(x) blablabla...: n=4 ");
+        printf("Please enter f1(x) blablabla...: n=4: ");
         for(int i=0; i<N; i++){
             scanf("%d", &f1Arr[i]);
             getchar();
@@ -54,7 +55,7 @@ int main(){
         }
         scanf("%d", &f1Arr[4]);
 
-        printf("Please enter f2(x) blablabla...: n=4 ");
+        printf("Please enter f2(x) blablabla...: n=4: ");
         for(int i=0; i<N; i++){
             scanf("%d", &f2Arr[i]);
             getchar();
@@ -62,17 +63,39 @@ int main(){
         }
         scanf("%d", &f2Arr[4]);
 
-        printf("F1(x)=");
-        for(int i=4; i>0; i--){
-            printf(" %dx^%d", f1Arr[i], i);
+        printf("F1(x)= %dx^%d", f1Arr[4], N);
+        for(int i=N-1; i>0; i--){
+            if(f1Arr[i] >= 0){
+                printf(" + %dx^%d", f1Arr[i], i);
+            }
+            else{
+                printf(" - %dx^%d", abs(f1Arr[i]), i);
+            }
         }
-        printf(" %d,\n", f1Arr[0]);
+        if(f1Arr[0] >= 0){
+            printf(" + %d", f1Arr[0]);
+        }
+        else{
+            printf(" - %d", abs(f1Arr[0]));
+        }
+        printf("\n");
 
-        printf("F2(x)=");
-        for(int i=4; i>0; i--){
-            printf(" %dx^%d", f2Arr[i], i);
+        printf("F2(x)= %dx^%d", f2Arr[4], N);
+        for(int i=N-1; i>0; i--){
+            if(f2Arr[i] >= 0){
+                printf(" + %dx^%d", f2Arr[i], i);
+            }
+            else{
+                printf(" - %dx^%d", abs(f2Arr[i]), i);
+            }
         }
-        printf(" %d\n", f2Arr[0]);
+        if(f2Arr[0] >= 0){
+            printf(" + %d", f2Arr[0]);
+        }
+        else{
+            printf(" - %d", abs(f2Arr[0]));
+        }
+        printf("\n");
 
 
         arrMultipl();
