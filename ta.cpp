1c476d0c8e601f7cf24ae463cf26db336e824436
Alec Wu <980124jack@gmail.com>
2022-05-24 11:11:12 +0800

    tets. May 24, 2022

--- ta.cpp
+++ ta.cpp
@@ -1,53 +1,27 @@
 #include <iostream>
-#include <string>
 using namespace std;
-
-unsigned Fibonacci[24][2] = { { 1, 1 } };
-string BFSs[4] = { "0", "1", "01", "101" };
-
-void Initialize() {
-	for (int i = 1; i < 24; ++i) {
-		Fibonacci[i][0] = Fibonacci[i - 1][0] + Fibonacci[i - 1][1];
-		Fibonacci[i][1] = Fibonacci[i - 1][1] + Fibonacci[i][0];
-	}
-}
-
+ 
 int main() {
-	cin.sync_with_stdio(false); cin.tie(nullptr);
-	long long times, number, left, right, nowIndex, buffer;
-	unsigned sums;
-	Initialize();
-	cin >> times;
-	while (times--) {
-		cin >> number >> left >> right;
-
-		if (number > 47)
-			number = 46 + (number % 2); // n = 46 + (k % 2)
-
-		for (int i = left; i <= right; i++) {
-			nowIndex = i;
-            buffer = number;
-
-			while (buffer >= 4) { // buffer = n
-				sums = 0;
-				if (buffer % 2) {
-					if (nowIndex < Fibonacci[(buffer / 2) - 1][1]) {
-						buffer -= 2;
-						continue;
-					}
-					sums += Fibonacci[(buffer / 2) - 1][1];
-				}
-				for (int j = 0; j < 2; j++) {
-					if (nowIndex < sums + Fibonacci[(buffer / 2) - 1][j]) {
-						buffer = (buffer - 2 - (buffer % 2)) + j;
-						nowIndex -= sums;
-						break;
-					}
-					sums += Fibonacci[(buffer / 2) - 1][j];
-				}
-			}
-			cout << BFSs[buffer][nowIndex];
-		}
-		cout << '\n';
-	}
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n1, n2;
+    while (cin >> n1 >> n2){
+        int carry = 0, cnt = 0;
+        if (n1 == 0 && n2 == 0) break;
+        while (n1 > 0 || n2 > 0){
+            int tmp = n1 % 10 + n2 % 10 + carry;
+            if (tmp >= 10){
+                carry = tmp / 10;
+                cnt++;
+            } else {
+                carry = 0;
+            }
+            n1 /= 10;
+            n2 /= 10;
+        }
+        if (cnt == 0) cout << "No carry operation.\n";
+        else if (cnt == 1) cout << "1 carry operation.\n";
+        else cout << cnt << " carry operations.\n";
+    }
+    return 0;
 }
