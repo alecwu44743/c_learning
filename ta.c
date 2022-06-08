d2418af7adec2c5f50892c94580a25e4a04789e0
Alec Wu <980124jack@gmail.com>
2022-06-08 17:57:50 +0800

    ta-ta.c June 8, 2022

--- ta.c
+++ ta.c
@@ -1,31 +1,25 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-#include<ctype.h>
 
-void guess(char ans){
-    char input;
+int main(void){
+    long long int v1, d1, v2, d2, count=1;
 
-    while(scanf("%c", &input)){
-             
-        if(input < ans){
-            printf("%s\n", "TOO SMALL!");
+    while(1){
+        scanf("%lld%lld%lld%lld", &v1, &d1, &v2, &d2);
+        if(d1==d2 && v1==v2 && d1==v1 && d1==0) break;
+        if(d1/v1 < d2/v2)   printf("Case #ll%d: You owe me a beer!\n", count);
+        else    printf("Case #%lld: No beer for the captain.\n", count);
+        long long int a=d1*v2+d2*v1, b=2*v1*v2, A=a, B=b, x;
+        if(a%b == 0){
+            printf("Avg. arrival time: %lld\n", a/b);
+        }else {
+            while(A>0 && B>0){
+                if(A>b) A=A%B;
+                else    B=B%A;
+            }
+            if(A==0)    x=B;
+            else x=A;
+            printf("Avg. arrival time: %lld/%lld\n", a/x, b/x);
         }
-        else if(input > ans){
-            printf("%s\n", "TOO BIG!");
-        }
-        else{
-            printf("%s\n", "GOOD!");
-            break;
-        }
-        getchar();
+        count++;
     }
-}
-
-int main(){
-    char str[4] = {'C', 'D', 'H', 'S'};
-    int i = rand()%4;
-
-    guess(str[i]);
 }
