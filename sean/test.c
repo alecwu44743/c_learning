9e488b8c0fb043767abc7822440fd391b15efa5b
Alec Wu <980124jack@gmail.com>
2022-06-16 15:53:23 +0800

    test June 16, 2022

--- sean/test.c
+++ sean/test.c
@@ -20,4 +20,15 @@ int main(){
     for(int i=0; i<10; i++){
         printf("%d\n", num.n[i]);
     }
+
+    char str[20] = "D=C[0:2,1:3]";
+    char dd;
+    int a, aa, b, bb;
+
+
+    scanf("%s", str); // D=C[0:2,1:3]
+    sscanf(str, "%c=%c[%d:%d,%d:%d]", &dd, &dd, &a, &aa, &b, &bb);
+
+    printf("%c %d %d %d %d\n", dd, a, aa, b, bb);
+
 }
