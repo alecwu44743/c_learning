07cc75515a5409901b272f4025ee6c836a04ece1
alecwu44743 <alec.wu44743@gmail.com>
2022-10-23 12:37:01 +0800

    fixed

--- fcu_cs/22fall_Data_Structures/DS_HW1-EightQueens/queen.cpp
+++ fcu_cs/22fall_Data_Structures/DS_HW1-EightQueens/queen.cpp
@@ -40,21 +40,35 @@ void print_ansQueens(){
 
 
 bool checkQueen(char queens[10][10], int row, int col){
-    for(int i=0; i<row; i++) {
-        if(queens[i][col]=='Q')
+    for(int i=0; i<R; i++) {
+        if(queens[i][col]=='Q' && i != row)
             return false;
     }
 
-    for(int i=row-1, j=col+1; i>=0 && j<C; i--, j++) {
+    for(int i=0; i<C; i++){
+        if(queens[row][i]=='Q' && i != col)
+            return false;
+    }
+
+    for(int i=row-1, j=col+1; i>=0 && j<C; i--, j++) { // 1
         if(queens[i][j]=='Q')
             return false;
     }
 
-    for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--) {
+    for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--) { // 2
         if(queens[i][j]=='Q')
             return false;
     }
 
+    for(int i=row+1, j=col+1; i<R && j<C; i++, j++) { // 4
+        if(queens[i][j]=='Q')
+            return false;
+    }
+
+    for(int i=row+1, j=col-1; i<R && j>=0; i++, j--) { // 3
+        if(queens[i][j]=='Q')
+            return false;
+    }
 
     return true;
 }
@@ -71,21 +85,53 @@ bool isPrepPlaced(int r, int c){
     return false;
 }
 
+int howManyQueens(char queens[10][10]){
+    int count = 0;
+    for(int i = 0; i < R; i++){
+        for(int j = 0; j < C; j++){
+            if(queens[i][j] == 'Q'){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 void placeQueen(char queens[10][10], int row, int cnt) {
-    // cout << "cnt: " << cnt << "maxQueens:" << maxQueens << endl;
-    if(cnt>maxQueens) {
-        // printQueen(queens);
-        maxQueens = cnt;
+    // cout << "  cnt: " << cnt << "maxQueens:" << maxQueens << endl;
+    cout << "==="  << "row: " << row << endl;
+    printQueen(queens);
+    cout << "===" << endl;
+
+    int QQ = howManyQueens(queens);
+    if(QQ > maxQueens){
+        // printQueen(queense);
+        maxQueens = QQ;
         for(int i=0; i<R; i++) {
             for(int j=0; j<C; j++) {
                 ansQueens[i][j] = queens[i][j];
             }
         }
+
+        // cout << "maxQueens: " << maxQueens << endl;
+        // print_ansQueens();
+
+        // print_ansQueens();
+        // printQueen(queens);
+        // cout << "maxQueens: " << maxQueens << endl;
+    }
+
+    if(row == R) {
         return;
     }
+
+    // bool nexted = false;
     for(int j=0; j<C; j++) {
+        printQueen(queens);
         if(checkQueen(queens, row, j) && !isPrepPlaced(row, j)) {
+            cout << "row" << row << "j" << j << endl;
             queens[row][j]='Q';
+            // nexted = true;
             placeQueen(queens, row+1, cnt+1);
             queens[row][j]='.';
         }
@@ -120,10 +166,14 @@ int main() {
         // print_ansQueens();
         
 
-        placeQueen(queens, 0, 0);
+        for(int i=0; i<R; i++) {
+            // printQueen(queens);
+            placeQueen(queens, i, 0);
+        }
         // cout << "_____" << endl;
         print_ansQueens();
         cout << maxQueens << endl;
+        // printQueen(queens);
     }
 
     
@@ -131,6 +181,8 @@ int main() {
     return 0;
 }
 
-// 5 3 2
-// 3 2
-// 4 2
+/*
+5 3 2
+3 2
+4 2
+*/
